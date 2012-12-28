543ee5fb6bf61160c7655000fa49e85f8abce801
tphoney@gmail.com <tphoney@gmail.com@c1b800a6-b4a8-a8e8-8dc7-5272425d1513>
2012-12-28 20:36:36 +0000

    Moved the http request of into its own async class

--- XBMCContentCompare/src/com/android/xbmccontentcompare/MainActivity.java
+++ XBMCContentCompare/src/com/android/xbmccontentcompare/MainActivity.java
@@ -1,42 +1,31 @@
 package com.android.xbmccontentcompare;
 
 import java.util.ArrayList;
-import java.util.List;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicHeader;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.protocol.HTTP;
-import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.os.StrictMode;
 import android.view.Menu;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class MainActivity extends Activity {
-	
+import com.android.xbmccontentcompare.XbmcRequest.MyCallbackInterface;
+
+public class MainActivity extends Activity implements MyCallbackInterface {
+
 	ArrayList<String> arraylist = new ArrayList<String>();
 	ArrayAdapter<String> adapter;
-	 
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+		// StrictMode.ThreadPolicy policy = new
+		// StrictMode.ThreadPolicy.Builder()
+		// .permitAll().build();
 
-		StrictMode.setThreadPolicy(policy);
+		// StrictMode.setThreadPolicy(policy);
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		final ListView listview = (ListView) findViewById(R.id.listScannedItems);
@@ -45,8 +34,10 @@ public class MainActivity extends Activity {
 		adapter = new ArrayAdapter<String>(this,
 				android.R.layout.simple_list_item_1, arraylist);
 		listview.setAdapter(adapter);
-		
-		getInfoFromProvider("");
+
+		// getInfoFromProvider("");
+		XbmcRequest parser = new XbmcRequest(this);
+		parser.execute("VideoLibrary.GetMovies", "title", "imdbnumber");
 	}
 
 	@Override
@@ -55,53 +46,29 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
-	
-	public void getInfoFromProvider(String upc) {
-		final String url = "http://192.168.0.5:80/jsonrpc";
-		
 
-		String googleResponse = "";
-		try {
-			HttpClient client = new DefaultHttpClient();
-			HttpPost request = new HttpPost(url);
-			request.addHeader("Content-Type", "application/json");
-			JSONObject json = new JSONObject();
-			json.put("jsonrpc", "2.0");
-			json.put("method", "VideoLibrary.GetMovies");
-			json.put("id", "1");
-			
-			StringEntity se = new StringEntity( json.toString());  
-            se.setContentType(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
-            request.setEntity(se);
-			
-			HttpResponse res = client.execute(request);
-
-			if (res.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
-				googleResponse = EntityUtils.toString(res.getEntity(), "UTF-8");
-			}
-
-		} catch (Exception e) {
-			System.out.println("Exp=" + e);
-		}
-
-		JSONObject jObject;
-		try {
-			jObject = new JSONObject(googleResponse);
-			JSONObject result = jObject.getJSONObject("result");
-			
-			JSONArray movies = result.getJSONArray("movies");
-			for (int i =0 ; i < movies.length(); i++) {
-				addnewEntry(movies.getJSONObject(i).getString("label"));
-			}
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
 	public void addnewEntry(String entry) {
 		arraylist.add(0, entry);
 		adapter.notifyDataSetChanged();
+	}
+
+	@Override
+	public void onRequestComplete(JSONObject result) {
+		JSONObject object;
+		if (null != result) {
+			try {
+				object = result.getJSONObject("result");
+
+				JSONArray movies = object.getJSONArray("movies");
+				for (int i = 0; i < movies.length(); i++) {
+					addnewEntry(movies.getJSONObject(i).getString("label"));
+				}
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
 	}
 
 }
---
+++ XBMCContentCompare/src/com/android/xbmccontentcompare/XbmcRequest.java
@@ -0,0 +1,94 @@
+package com.android.xbmccontentcompare;
+
+import java.util.ArrayList;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.os.AsyncTask;
+
+public class XbmcRequest extends AsyncTask<String, Void, JSONObject> {
+
+	final String url = "http://192.168.0.5:80/jsonrpc";
+
+	public interface MyCallbackInterface {
+		public void onRequestComplete(JSONObject result);
+	}
+
+	private MyCallbackInterface mCallback;
+
+	public XbmcRequest(MyCallbackInterface callback) {
+		mCallback = callback;
+	}
+
+	@Override
+	protected JSONObject doInBackground(String... params) {
+		return getJSONFromXbmcMethod(params);
+	}
+
+	@Override
+	protected void onPostExecute(JSONObject result) {
+		// In here, call back to Activity or other listener that things are done
+		mCallback.onRequestComplete(result);
+	}
+
+	protected JSONObject getJSONFromXbmcMethod(final String... params) {
+		String httpResponseContents = "";
+		if (params.length > 0) {
+			try {
+				HttpClient client = new DefaultHttpClient();
+				HttpPost httpPost = new HttpPost(url);
+				httpPost.addHeader("Content-Type", "application/json");
+				JSONObject jsonObjectPost = new JSONObject();
+				jsonObjectPost.put("jsonrpc", "2.0");
+				jsonObjectPost.put("method", params[0]);
+				if (2 <= params.length) {
+					JSONArray jsonArrayProperties = new JSONArray();
+					for (int i = 1; i < params.length; i++) {
+						jsonArrayProperties.put(params[i]);
+					}
+
+					JSONObject jsonObjectProperties = new JSONObject();
+					jsonObjectProperties.put("properties", jsonArrayProperties);
+
+					jsonObjectPost.put("params", jsonObjectProperties);
+				}
+				jsonObjectPost.put("id", "1");
+
+				StringEntity stringEntity = new StringEntity(jsonObjectPost.toString());
+				stringEntity.setContentType(new BasicHeader(HTTP.CONTENT_TYPE,
+						"application/json"));
+				httpPost.setEntity(stringEntity);
+
+				HttpResponse res = client.execute(httpPost);
+
+				if (res.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+					httpResponseContents = EntityUtils.toString(res.getEntity(),
+							"UTF-8");
+				}
+
+			} catch (Exception e) {
+				System.out.println("Exp=" + e);
+			}
+		}
+		JSONObject returnedJson = null;
+		try {
+			returnedJson = new JSONObject(httpResponseContents);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return returnedJson;
+	}
+
+}
