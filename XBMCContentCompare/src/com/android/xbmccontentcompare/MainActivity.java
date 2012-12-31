406fafe8ee63628a5f2bdc2ab65bba50091d7204
tphoney@gmail.com <tphoney@gmail.com@c1b800a6-b4a8-a8e8-8dc7-5272425d1513>
2012-12-31 14:04:10 +0000

    Working statuses on the mainscreen. now scans remote xbmc

--- XBMCContentCompare/src/com/android/xbmccontentcompare/MainActivity.java
+++ XBMCContentCompare/src/com/android/xbmccontentcompare/MainActivity.java
@@ -11,6 +11,9 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 
 import com.android.xbmccontentcompare.XbmcRequest.MyCallbackInterface;
 
@@ -21,19 +24,61 @@ public class MainActivity extends Activity implements MyCallbackInterface {
 	public VideoLibrary resultsLibrary = new VideoLibrary();
 	String homeIp;
 	String homePort;
+	Boolean homeRequest = true;
+	CheckBox homeXbmcStatus, remoteXbmcStatus;
+	Button btnDuplicates, btnScanRemote;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		// prefs and screen init
 		SharedPreferences prefs = PreferenceManager
 				.getDefaultSharedPreferences(MainActivity.this);
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 		homeIp = prefs.getString("IP", "192.168.0.1");
 		homePort = prefs.getString("Port", "80");
 
-		remoteLibrary.addMovie(new Movie("500 Days of Summer", "tt1022603"));
+		// remoteLibrary.addMovie(new Movie("500 Days of Summer", "tt1022603"));
+		// ui work
+		homeXbmcStatus = (CheckBox) findViewById(R.id.checkBoxHomeXBMC);
+		remoteXbmcStatus = (CheckBox) findViewById(R.id.checkBoxRemoteXBMC);
+		btnDuplicates = (Button) findViewById(R.id.btnDuplicates);
 
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		
+		// buttons and listeners = need to clean up
+		btnDuplicates.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				resultsLibrary = remoteLibrary.findDuplicates(homeLibrary);
+				Intent displayResultsIntent = new Intent(MainActivity.this,
+						DisplayResultsActivity.class);
+				displayResultsIntent.putExtra("VideoLibrary", resultsLibrary);
+				startActivity(displayResultsIntent);
+			}
+		});
+
+		btnScanRemote = (Button) findViewById(R.id.btnScanRemote);
+		btnScanRemote.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				homeRequest = false;
+				XbmcRequest parser = new XbmcRequest(MainActivity.this,
+						"192.168.0.3", homePort);
+				parser.execute("VideoLibrary.GetMovies", "title", "imdbnumber");
+			}
+		});
+		// update status
+		updateStatuses();
+	}
+
+	private void updateStatuses() {
+		if (homeLibrary.movies.size() > 0) {
+			homeXbmcStatus.setChecked(true);
+		} else {
+			homeXbmcStatus.setChecked(false);
+		}
+		if (remoteLibrary.movies.size() > 0) {
+			remoteXbmcStatus.setChecked(true);
+		} else {
+			remoteXbmcStatus.setChecked(false);
+		}
 	}
 
 	@Override
@@ -51,15 +96,9 @@ public class MainActivity extends Activity implements MyCallbackInterface {
 			startActivity(new Intent(this, PrefsActivity.class));
 			return true;
 		case R.id.menu_scan_home:
+			homeRequest = true;
 			XbmcRequest parser = new XbmcRequest(this, homeIp, homePort);
 			parser.execute("VideoLibrary.GetMovies", "title", "imdbnumber");
-			return true;
-		case R.id.menu_duplicates:
-			resultsLibrary = remoteLibrary.findDuplicates(homeLibrary);
-			Intent displayResultsIntent = new Intent(this,
-					DisplayResultsActivity.class);
-			displayResultsIntent.putExtra("VideoLibrary", resultsLibrary);
-			startActivity(displayResultsIntent);
 			return true;
 		}
 		return false;
@@ -68,20 +107,27 @@ public class MainActivity extends Activity implements MyCallbackInterface {
 	@Override
 	public void onRequestComplete(JSONObject result) {
 		JSONObject object;
+		VideoLibrary tmpLibrary = new VideoLibrary();
 		if (null != result) {
 			try {
 				object = result.getJSONObject("result");
 				JSONArray movies = object.getJSONArray("movies");
 				for (int i = 0; i < movies.length(); i++) {
 					// addnewEntry(movies.getJSONObject(i).getString("label"));
-					homeLibrary.addMovie(new Movie(movies.getJSONObject(i)
+					tmpLibrary.addMovie(new Movie(movies.getJSONObject(i)
 							.getString("label"), movies.getJSONObject(i)
 							.getString("imdbnumber")));
+				}
+				if (homeRequest) {
+					homeLibrary = tmpLibrary;
+				} else {
+					remoteLibrary = tmpLibrary;
 				}
 			} catch (JSONException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
+		updateStatuses();
 	}
 }
